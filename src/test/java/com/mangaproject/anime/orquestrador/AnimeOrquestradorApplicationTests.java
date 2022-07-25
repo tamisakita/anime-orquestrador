c911429e036f4b1be20df9e10423d99b60bce898
Patricia Tami Sakita <67209629+tamisakita@users.noreply.github.com>
2022-07-25 12:27:43 -0300

    Initial commit

---
+++ src/test/java/com/mangaproject/anime/orquestrador/AnimeOrquestradorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mangaproject.anime.orquestrador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeOrquestradorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
